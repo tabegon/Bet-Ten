8903b5e689a4455e268d4e2dd1e2d4d279083e11
tabegon <theo.abegon@gmail.com>
2024-11-18 19:06:19 +0100

    login a moitié fonctionnel

---
+++ js/login.js
@@ -0,0 +1,11 @@
+const users = [{nom: "Th30", password: 'tkt' }];
+
+function enter(){
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+
+    if (usernameInput.value.trim() != '' &&
+        passwordInput.value.trim() != '') {
+            document.location.href='main.html'}
+}
+
