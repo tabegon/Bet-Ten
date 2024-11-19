6a7187373eb955bd79ca2781c2267dce96a24aaa
tabegon <theo.abegon@gmail.com>
2024-11-19 22:01:46 +0100

    ajouts des pages intermédiaires login

--- js/code.js
+++ js/code.js
@@ -2,32 +2,44 @@
 setInterval(() => document.getElementById('clock').innerText = new Date().toLocaleTimeString(), 1000);
 
 // Fonction pour afficher une animation après un pari
-function showAnimation(result) {
+function showAnimation(result, correctAnswer) {
     const anim = document.getElementById('animation');
     anim.style.display = 'block';
-    anim.innerText = result ? "Gagné !" : "Perdu !";
+    anim.innerHTML = result ? "Gagné !" : "Perdu !";
+    if (anim.innerHTML == "Perdu !"){
+        anim.innerHTML = "Perdu ! " + '<br>' +  "la réponse était " + correctAnswer 
+    }
     setTimeout(() => anim.style.display = 'none', 2000);
-}
+    
+};
 
 // Fonction de vérification des réponses au quiz
 function checkAnswer(button, correctAnswer) {
     const input = button.previousElementSibling.value;
     const questionDiv = button.parentElement;
-    if (input.toLowerCase() === correctAnswer.toLowerCase()) {
-        showAnimation(true);
+    if (input.toLowerCase() == correctAnswer.toLowerCase()) {
+        showAnimation(true, correctAnswer);
     } else {
-        showAnimation(false);
+        showAnimation(false, correctAnswer);
     }
     questionDiv.style.display = "none"; // Remplacer la question
-}
+};
 
 const users = {nom: "Th30", password: 'tkt', points: 10 };
 
 function enter(){
-    const usernameInput = document.getElementById('username');
-    const passwordInput = document.getElementById('password');
+    console.log('test')
+    var usernameInput = document.getElementById('name');
+    var passwordInput = document.getElementById('mdp');
 
-    if (usernameInput.value == users.nom &&
-        passwordInput.value == users.password) {
-            document.location.href='main.html'}
+    if (typeof usernameInput !== "undefined" &&
+        typeof passwordInput !== "undefined") {
+        if (usernameInput.value == users.nom &&
+            passwordInput.value == users.password) {
+                document.location.href='main.html'}
+}
+}
+
+function to_login(){
+    document.location.href='login.html'
 }
