cceec8ec68b9e53c0b5c321cb92073e4a414e5fc
tabegon <theo.abegon@gmail.com>
2024-11-27 09:16:17 +0100

    Amélioration de la page login

--- js/code.js
+++ js/code.js
@@ -3,41 +3,44 @@ setInterval(() => document.getElementById('clock').innerText = new Date().toLoca
 
 // Fonction pour afficher une animation après un pari
 function showAnimation(result, correctAnswer) {
-    const anim = document.getElementById('animation');
-    anim.style.display = 'block';
-    anim.innerHTML = result ? "Gagné !" : "Perdu !";
-    if (anim.innerHTML == "Perdu !"){
+    const anim = document.getElementById('animation');  // Récupère l'élément par l'id 'animation' et le met dans la constante anim
+    anim.style.display = 'block';                       // Affiche l'élément 
+    anim.innerHTML = result ? "Gagné !" : "Perdu !";    // Change le texte html de l'élément en fonction du résultat (Gagné ou Perdu)
+    if (anim.innerHTML == "Perdu !"){                   // Si le texte html d'anim est "perdu":
         anim.innerHTML = "Perdu ! <br> la réponse était " + correctAnswer 
-    }
-    setTimeout(() => anim.style.display = 'none', 2000);
+    }                                                   // Change le texte html d'anim pour avoir la réponse avec
+    setTimeout(() => anim.style.display = 'none', 2000);// Fait disparaître l'élément au bout d'un certain temps
     
 };
 
 // Fonction de vérification des réponses au quiz
 function checkAnswer(button, correctAnswer) {
-    const input = button.previousElementSibling.value;
-    const questionDiv = button.parentElement;
-    if (input.toLowerCase() == correctAnswer.toLowerCase()) {
-        showAnimation(true, correctAnswer);
-    } else {
-        showAnimation(false, correctAnswer);
-    }
-    questionDiv.style.display = "none"; // Remplacer la question
+    const input = button.previousElementSibling.value;          // Prends la valeur de l'input de la question et la met dans la constante input
+    const questionDiv = button.parentElement;                   // Prends la division de la question et la met dans la constante questionDiv
+    if (input.toLowerCase() == correctAnswer.toLowerCase()) {   // Si la valeur de l'input est = à la réponse
+        showAnimation(true, correctAnswer);                     // Utilise la fonction showAnimation avec comme parmètre: True (bonne réponse)
+    } else {                                                        // et le paramètre correctAnswer qui correspond à la réponse
+        showAnimation(false, correctAnswer);                    // Utilise la fonction showAnimation avec comme parmètre: False (fausse réponse)
+    }                                                               // et le paramètre correctAnswer qui correspond à la réponse
+    questionDiv.style.display = "none"; // Supprime la question
 };
 
-const users = {nom: "Th30", password: 'tkt', points: 10 }
+const users = {nom: "Th30", password: 'tkt', points: 10 }       // Tous les utilisateurs avec leurs mdp et leurs nb de points
+var passwordInput = document.forms['formulaire']['mdp'].value;  // Récupère la valeur de l'input mdp
+var usernameInput = document.forms['formulaire']['name'].value; // Récupère la valeur de l'input name
 
+// Fonction d'entrée dans le site
 function enter(){
-    var passwordInput = document.forms['formulaire']['mdp'].value;
-    var usernameInput = document.forms['formulaire']['name'].value;
-    if (usernameInput == users.nom &&
-        passwordInput == users.password) {
-            open('main.html')};
+        if (usernameInput === users.nom &&      // Si la valeur de nom est = au nom de users et
+            passwordInput === users.password) {     // la valeur de mdp est = au mdp de users
+                open('main.html');              // Ouvre la page web voulue
+                close('login.html')};           // Fermer la page actuelle
 }
 
-
+// Fonction qui amène la page login
 function to_login(){
-    if (usernameInput.value !== "" &&
-        passwordInput.value !== "") {
-            document.location.href='login.html'}
+    if (usernameInput !== "" &&     // Si la valeur de nom est différent à rien et
+        passwordInput !== "") {         // la valeur de mdp est différente à rien
+            open('login.html');     // Ouvre la page login
+            close('register.html')} // Ferme la page actuelle
 }
