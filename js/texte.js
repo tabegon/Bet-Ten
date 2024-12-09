44dd8fe4a97043b2a721d7ab3a1b541168e5f5ff
tabegon <theo.abegon@gmail.com>
2024-12-09 22:21:51 +0100

    page auto

---
+++ js/texte.js
@@ -0,0 +1,4 @@
+const Jannik = {titre: 'Jannik Sinner', 
+                intro: 'Jannik Sinner est un joueur de tennis professionnel italien, connu pour son style de jeu agressif et sa maturité impressionnante sur le court malgré son jeune âge. Né le 16 août 2001 à San Candido, dans le Tyrol du Sud, il est considéré comme l’un des talents les plus prometteurs de sa génération.',
+                car_phys: '<strong>Taille :</strong> 1,88 m</li><li><strong>Poids :</strong> 76 kg</li><li><strong>Jeu :</strong> Droitier, revers à deux mains',
+                }
