10befdbe23e6a07e1204064afc242dd334501e85
tabegon <theo.abegon@gmail.com>
2024-12-14 18:39:29 +0100

    creation base de donnees

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+import sqlite3
 from flask import Flask, redirect
 
+def create_database():
+    conn = sqlite3.connect('tennis.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users (
+              id INTEGER PRIMARY KEY AUTOINCREMENT, 
+              prenom VARCHAR(50),
+              nom VARCHAR(100),
+              username VARCHAR(50) UNIQUE,
+              password VARCHAR(255),
+              points INTEGER 
+               )''')
+    conn.commit()
+    conn.close()
+
+
+create_database()
 app = Flask(__name__,
             static_url_path='',
             static_folder='app/static')
+
 
 @app.route("/")
 def index():
