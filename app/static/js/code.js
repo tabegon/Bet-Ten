9fbad4a41ab084349a5731a87977927bea8a7d9b
tabegon <theo.abegon@gmail.com>
2024-12-14 22:16:24 +0100

    actualisation code javascript

--- app/static/js/code.js
+++ app/static/js/code.js
@@ -32,21 +32,3 @@ function checkAnswer(button, correctAnswer) {
     }                                                               // et le paramètre correctAnswer qui correspond à la réponse
     questionDiv.style.display = "none"; // Supprime la question
 };
-
-const users = {nom: "Th30", password: 'tkt', points: 10 };      // Tous les utilisateurs avec leurs mdp et leurs nb de points
-
-// Fonction d'entrée dans le site
-function enter(passwordInput, usernameInput){
-    if (usernameInput == users.nom &&       // Si la valeur de nom est = au nom de users et
-        passwordInput == users.password) {  // la valeur de mdp est = au mdp de users
-            open('main.html');              // Ouvre la page web voulue
-            close('login.html')};           // Fermer la page actuelle
-};
-
-// Fonction qui amène la page login
-function to_login(passwordInput, usernameInput){
-    if (usernameInput !== "" &&     // Si la valeur de nom est différent à rien et
-        passwordInput !== "") {         // la valeur de mdp est différente à rien
-            open('login.html');     // Ouvre la page login
-            close('register.html')} // Ferme la page actuelle
-}
